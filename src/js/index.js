c9966dc1e24665887550944d45d04e66ac21c1da
Alexander Frick <Alex313031@gmail.com>
2023-09-21 23:19:15 -0500

    fix inject script

--- src/js/index.js
+++ src/js/index.js
@@ -4,7 +4,7 @@ or remove elements as needed.
 
 /* Clean up the "Open in Music" div */
 function removeElementsByClass(className) {
-  const elements = document.getElementsByClassName("svelte-agv6qns");
+  const elements = document.getElementsByClassName("svelte-agv6qn");
   while(elements.length > 0){
     elements[0].parentNode.removeChild(elements[0]);
   }
